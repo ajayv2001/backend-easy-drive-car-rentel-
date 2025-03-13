c0ee9f2d1b86ba746a478cc3450795916126c625
AJAY <ajayvnair61@gmail.com>
2025-03-13 11:54:22 +0530

    to redis issue solved

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,9 +1,12 @@
 import Redis from "ioredis"
+import dotenv from 'dotenv';
+
+dotenv.config(); 
 
 // Initialize Redis Client
 const redisClient = new Redis({
-  host: "127.0.0.1", // Redis server hostname
-  port: 6379,        // Redis server port
+  host:process.env.REDIS_HOST , // Redis server hostname
+  port: process.env.REDIS_PORT,        // Redis server port
   // password: "your_redis_password", // Uncomment if Redis requires authentication
 });
 
